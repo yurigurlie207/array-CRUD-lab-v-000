5b0df26a3c7299adc601a0548f101f9071baf05d
Adrienne Yao <adrienne.yao@icloud.com>
2017-10-31 18:30:17 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,11 +8,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.unshift(element)
+
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.push(element)
+    array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
